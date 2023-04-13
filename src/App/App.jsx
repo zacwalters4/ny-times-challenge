01d9544c2da397d6ffeffd4d5df17810aae420ca
Zac Walters <zacwalters4@gmail.com>
2023-04-12 18:16:38 -0600

    Add article and app folders

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import Article from '../Article/Article'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      <Article />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Article/Article.jsx
@@ -0,0 +1,11 @@
+import './Article.css';
+
+function Article() {
+  return (
+    <div className="Article">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Article;
